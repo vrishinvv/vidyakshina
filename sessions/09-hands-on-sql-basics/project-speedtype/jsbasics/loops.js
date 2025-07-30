8f84f360eabae356c69accc41e549b293f480ba5
Ani <saianrudhs@gmail.com>
2025-07-30 19:20:53 +0530

    files latest

---
+++ sessions/09-hands-on-sql-basics/project-speedtype/jsbasics/loops.js
@@ -0,0 +1,14 @@
+// 🔁 Loops
+// Repeat something again and again.
+
+// FOR LOOP: Repeat a fixed number of times
+for (let i = 1; i <= 5; i++) {
+  console.log("Hello " + i);
+}
+
+// WHILE LOOP: Repeat until a condition is false
+let count = 1;
+while (count <= 3) {
+  console.log("Count is: " + count);
+  count++;
+}
---
+++ sessions/11-DB-operations-crud-api/project-speedtype/jsbasics/functions.js
@@ -0,0 +1,9 @@
+// 🧠 Functions
+// A block of code we can use again and again
+
+function greet(name) {
+  console.log("Hello, " + name);
+}
+
+greet("Anirudh");
+greet("Sanjay");
